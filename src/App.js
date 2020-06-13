d3d76f3256c47e56d1b8e4fc24c6464f761f0d45
Daily120 <forzhacker@mail.ru>
2020-06-13 19:21:25 +0200

    feat/add-puzzle-game | Add puzzle game to the project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import PlayerHolder from './components/PlayerHolder/PlayerHolder';
 
+import { ImagesProvider } from "./contexts/ImagesContext";
+import { useTransition, animated } from "react-spring";
+
+const Puzzle = React.lazy(() => {
+  return import("./games/Puzzle/Puzzle");
+});
+
 function App() {
   return (
     <div className="App">
-      <PlayerHolder />
+      {/* <PlayerHolder /> */}
+      <animated.div key={"key"}>
+        <Suspense fallback={""}>
+          <ImagesProvider
+            r={require.context(
+              "./games/Puzzle/images/",
+              false,
+              /\.(png|jpe?g|svg)$/
+            )}
+          >
+            <Puzzle />
+          </ImagesProvider>
+        </Suspense>
+      </animated.div>
     </div>
   );
 }
