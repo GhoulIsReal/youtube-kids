87569a17644820d3fd400e7550bf857bf812b61e
ghoulIsReal <erasyl.boy@gmail.com>
2020-06-16 23:01:48 +0600

    dxdxdx

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import * as styled from './FooterStyles';
 
-const Footer = () => {
+const Footer = ({ url }) => {
     return(
-        <styled.FooterHolder>
+        <styled.FooterHolder style={{ position: url === '' ? 'absolute' : 'static', bottom: 0 }}>
             <styled.Footer>Copyright 2020 © KidsVi All Rights Reserved</styled.Footer>
         </styled.FooterHolder>
     )
