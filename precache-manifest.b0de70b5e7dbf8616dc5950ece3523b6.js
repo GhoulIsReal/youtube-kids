49bfc6f1e99c38328f911515d2ef974b55cbafe0
ghoulIsReal <erasyl.boy@gmail.com>
2020-06-18 22:25:06 +0600

    Updates

---
+++ precache-manifest.b0de70b5e7dbf8616dc5950ece3523b6.js
@@ -0,0 +1,242 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3c6b9a8a8c46f89a9cbfe3540b8b725",
+    "url": "/youtube-kids/index.html"
+  },
+  {
+    "revision": "8797873dcaff81a16b96",
+    "url": "/youtube-kids/static/css/18.435e924f.chunk.css"
+  },
+  {
+    "revision": "d117053f6310ab87ffd0",
+    "url": "/youtube-kids/static/css/19.43cd3e7e.chunk.css"
+  },
+  {
+    "revision": "1c70923f9232c79bca5e",
+    "url": "/youtube-kids/static/css/main.43b152c3.chunk.css"
+  },
+  {
+    "revision": "83f6744cb243b3e3ec93",
+    "url": "/youtube-kids/static/js/0.79055d65.chunk.js"
+  },
+  {
+    "revision": "fad1859d60af6fc09ad1",
+    "url": "/youtube-kids/static/js/1.09e74ae3.chunk.js"
+  },
+  {
+    "revision": "6c4055ade66c4b715b63",
+    "url": "/youtube-kids/static/js/16.9ffc5097.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/youtube-kids/static/js/16.9ffc5097.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4ab9c0a1b33d32d5c8f",
+    "url": "/youtube-kids/static/js/17.9b766e16.chunk.js"
+  },
+  {
+    "revision": "8797873dcaff81a16b96",
+    "url": "/youtube-kids/static/js/18.41721523.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/youtube-kids/static/js/18.41721523.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d117053f6310ab87ffd0",
+    "url": "/youtube-kids/static/js/19.04a2fb19.chunk.js"
+  },
+  {
+    "revision": "a6a8b1a1be72e91ae9b5",
+    "url": "/youtube-kids/static/js/20.c2e43846.chunk.js"
+  },
+  {
+    "revision": "1c70923f9232c79bca5e",
+    "url": "/youtube-kids/static/js/main.a770bef5.chunk.js"
+  },
+  {
+    "revision": "1f2ccdb77253d6897840",
+    "url": "/youtube-kids/static/js/reactPlayerDailyMotion.122be331.chunk.js"
+  },
+  {
+    "revision": "446464a7f09014627e6a",
+    "url": "/youtube-kids/static/js/reactPlayerFacebook.f70a7acd.chunk.js"
+  },
+  {
+    "revision": "41cd85553fc522da921d",
+    "url": "/youtube-kids/static/js/reactPlayerFilePlayer.ecd781ae.chunk.js"
+  },
+  {
+    "revision": "5c729659c32421e5e0fb",
+    "url": "/youtube-kids/static/js/reactPlayerMixcloud.06f904fb.chunk.js"
+  },
+  {
+    "revision": "f24d07baf9842325c09b",
+    "url": "/youtube-kids/static/js/reactPlayerPreview.ae34d9bf.chunk.js"
+  },
+  {
+    "revision": "2a1b4f6f7580259b590d",
+    "url": "/youtube-kids/static/js/reactPlayerSoundCloud.ee2830a2.chunk.js"
+  },
+  {
+    "revision": "4c30fbe82be3fc67aefc",
+    "url": "/youtube-kids/static/js/reactPlayerStreamable.f41f1003.chunk.js"
+  },
+  {
+    "revision": "f4488dbaacd5a780d9b4",
+    "url": "/youtube-kids/static/js/reactPlayerTwitch.b82aefc5.chunk.js"
+  },
+  {
+    "revision": "c296c8a65a9aab1c770c",
+    "url": "/youtube-kids/static/js/reactPlayerVidyard.8a9106d8.chunk.js"
+  },
+  {
+    "revision": "086a1810743b5adfa0f3",
+    "url": "/youtube-kids/static/js/reactPlayerVimeo.9b44cb4a.chunk.js"
+  },
+  {
+    "revision": "b2ebb9d68301df54308b",
+    "url": "/youtube-kids/static/js/reactPlayerWistia.ce4259cb.chunk.js"
+  },
+  {
+    "revision": "c7233628577f2a74e79b",
+    "url": "/youtube-kids/static/js/reactPlayerYouTube.317fd872.chunk.js"
+  },
+  {
+    "revision": "0233b5ee0fc80b72dee3",
+    "url": "/youtube-kids/static/js/runtime-main.44634268.js"
+  },
+  {
+    "revision": "07e121c52b58be658fb0acf23c741558",
+    "url": "/youtube-kids/static/media/1.07e121c5.png"
+  },
+  {
+    "revision": "9c511df87ea8083e3ed775c36df87ccb",
+    "url": "/youtube-kids/static/media/2.9c511df8.png"
+  },
+  {
+    "revision": "751ecb7ce981a85f1a5e72e168aca30c",
+    "url": "/youtube-kids/static/media/3.751ecb7c.png"
+  },
+  {
+    "revision": "7ef2a68531bb64c43a21d1268a98319b",
+    "url": "/youtube-kids/static/media/4.7ef2a685.png"
+  },
+  {
+    "revision": "add11ae6da8c1c6068bfc9d89139f348",
+    "url": "/youtube-kids/static/media/5.add11ae6.png"
+  },
+  {
+    "revision": "96037b82cd18cfb7b4ce188d97d8a771",
+    "url": "/youtube-kids/static/media/apple.96037b82.svg"
+  },
+  {
+    "revision": "b6f14cc52d3c98e60259a8122fc25580",
+    "url": "/youtube-kids/static/media/balloon.b6f14cc5.png"
+  },
+  {
+    "revision": "60460b3f3ad4474da60b8b9bd7c8539f",
+    "url": "/youtube-kids/static/media/balloons.60460b3f.svg"
+  },
+  {
+    "revision": "9a38de60b442711e9fd85bc51360fc98",
+    "url": "/youtube-kids/static/media/bananas.9a38de60.svg"
+  },
+  {
+    "revision": "e51e28c04f99b168c85e7ffec8800ed0",
+    "url": "/youtube-kids/static/media/bear.e51e28c0.svg"
+  },
+  {
+    "revision": "16aa08ebbf689edc8cdf9eef7df1da5a",
+    "url": "/youtube-kids/static/media/bg.16aa08eb.png"
+  },
+  {
+    "revision": "5c95bfbc0815bd3f514eb36227c242f3",
+    "url": "/youtube-kids/static/media/bg.5c95bfbc.png"
+  },
+  {
+    "revision": "75a8a496d7fe05bfb853ba894ff33b51",
+    "url": "/youtube-kids/static/media/broccoli.75a8a496.svg"
+  },
+  {
+    "revision": "ae4da5aee02409aac649e27927cb3cd5",
+    "url": "/youtube-kids/static/media/cabbage.ae4da5ae.svg"
+  },
+  {
+    "revision": "89cafa12ea2a4bbdae27d306bbf150d8",
+    "url": "/youtube-kids/static/media/cake.89cafa12.svg"
+  },
+  {
+    "revision": "7dfd44f205d28ec0f79c7bf4924f54d8",
+    "url": "/youtube-kids/static/media/carrot.7dfd44f2.svg"
+  },
+  {
+    "revision": "17206da723904cf912003e5eb253ef66",
+    "url": "/youtube-kids/static/media/croissant.17206da7.svg"
+  },
+  {
+    "revision": "30734eac185216390716ad43cca31cdb",
+    "url": "/youtube-kids/static/media/done.30734eac.svg"
+  },
+  {
+    "revision": "705c973bc0a54cde4248c321bff96bd0",
+    "url": "/youtube-kids/static/media/eggs.705c973b.svg"
+  },
+  {
+    "revision": "9b230f630f0bf27ac71883ee60d350bd",
+    "url": "/youtube-kids/static/media/filter.9b230f63.svg"
+  },
+  {
+    "revision": "11be6a407f71610d05cf35e895aab424",
+    "url": "/youtube-kids/static/media/grapes.11be6a40.svg"
+  },
+  {
+    "revision": "84db2cf4905748590a433e8fddf556d9",
+    "url": "/youtube-kids/static/media/jam.84db2cf4.svg"
+  },
+  {
+    "revision": "16426bdb5f7bc5776a6df8ff63b2dff4",
+    "url": "/youtube-kids/static/media/milk.16426bdb.svg"
+  },
+  {
+    "revision": "9c594fbbc2954a402815223d23161b12",
+    "url": "/youtube-kids/static/media/mustard.9c594fbb.svg"
+  },
+  {
+    "revision": "fb4880daa21ea087400ee69e783f8e36",
+    "url": "/youtube-kids/static/media/onion.fb4880da.svg"
+  },
+  {
+    "revision": "5a7f032e33fbff4ce480a0e086ed6fba",
+    "url": "/youtube-kids/static/media/orange.5a7f032e.svg"
+  },
+  {
+    "revision": "b7c510737e2c2a186fb5bbb45efd4df0",
+    "url": "/youtube-kids/static/media/pasta.b7c51073.svg"
+  },
+  {
+    "revision": "ce0b05ef979f7be2bc953f2a47c6766f",
+    "url": "/youtube-kids/static/media/pineapple.ce0b05ef.svg"
+  },
+  {
+    "revision": "2938dbf24241ec348f87895fba6a4b24",
+    "url": "/youtube-kids/static/media/potato.2938dbf2.svg"
+  },
+  {
+    "revision": "098fb5d3aac8e11f66d2ca5551ef107e",
+    "url": "/youtube-kids/static/media/shopping-cart.098fb5d3.svg"
+  },
+  {
+    "revision": "b7931bae80e691c7531de66c447bb572",
+    "url": "/youtube-kids/static/media/strawberry.b7931bae.svg"
+  },
+  {
+    "revision": "4826181a9b05a17c486fe9c57685211f",
+    "url": "/youtube-kids/static/media/tomato.4826181a.svg"
+  },
+  {
+    "revision": "9b004dbc24d07b07f8fe89399f7811ea",
+    "url": "/youtube-kids/static/media/watermelon.9b004dbc.svg"
+  }
+]);
